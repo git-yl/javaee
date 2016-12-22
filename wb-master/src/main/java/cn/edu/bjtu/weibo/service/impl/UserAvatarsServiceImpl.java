3c35630e2103f5fd64ea9a5912f4236db153109e
gitjavaee <14301164@bjtu.edu.cn>
2016-12-22 23:22:15 +0800

    hw

--- wb-master/src/main/java/cn/edu/bjtu/weibo/service/impl/UserAvatarsServiceImpl.java
+++ wb-master/src/main/java/cn/edu/bjtu/weibo/service/impl/UserAvatarsServiceImpl.java
@@ -5,37 +5,22 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import cn.edu.bjtu.weibo.dao.UserDAO;
+import cn.edu.bjtu.weibo.dao.impl.UserDAOImpl;
 import cn.edu.bjtu.weibo.model.Picture;
 import cn.edu.bjtu.weibo.service.UserAvatarsService;
-import redis.clients.jedis.Jedis;
 
 public class UserAvatarsServiceImpl implements UserAvatarsService {
 
 	@Override
 	public List<Picture> getUserAvatarList(String userId, int pageIndex, int numberPerPage) {
-		List<String> values = null;
 		List<Picture> pics = new ArrayList<Picture>();
-		Jedis jedis = null;
-		try{
-			jedis = new Jedis("127.0.0.1", 6379);
-			Long len = jedis.llen("PicurlOr");
-			if(len < pageIndex*numberPerPage){
-			}else if(len < (pageIndex+1)*numberPerPage){
-				values = jedis.lrange("PicurlOr", pageIndex*numberPerPage, len-1);
-			}else{
-				values = jedis.lrange("PicurlOr", pageIndex*numberPerPage, (pageIndex+1)*numberPerPage-1);
-			}
-			for(int i=0;i<values.size();i++){
-				Picture ap = new Picture();
-				ap.setPicurl(values.get(i));
-				pics.add(ap);
-			}
-		}catch (Exception e){
-			return null;
-		}finally{
-			if(jedis.isConnected()){
-				jedis.close();
-			}
+		UserDAO auser = new UserDAOImpl();
+		List<String> pictures = auser.getUserAvatars(userId, pageIndex, numberPerPage);
+		for(int i = 0; i < pictures.size();i++){
+			Picture apic = new Picture();
+			apic.setPicurl(pictures.get(i));
+			pics.add(apic);
 		}
 		return pics;
 	}
